f486fb1d52d8f956f537f09a0047c559524932a3
Heitor-Freixo <hspanofreixo@gmail.com>
2025-09-29 17:04:13 -0300

    bublle sort sem fazer pro enzo

---
+++ bublle.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+typedef struct{
+    char nome[100];
+}Nomes;
+int main(){
+    FILE*arquivo;
+    char linha[100], *token;
+    char temp[100];
+    int i=0;
+    Nomes nm[100];
+    arquivo=fopen("data_fixed.csv", "r");
+    if(arquivo==NULL){
+        printf("ERRO!!!!");
+        return 1;
+    }
+    while(fgets(linha, sizeof(linha), arquivo)){
+        token= strtok(linha, "\n");
+        strcpy(nm[i].nome, token);
+        i++;
+    }
+    printf("Total de registros lidos: %d\n", i);
+    fclose(arquivo);
+    for(int x=0;x<100;x++){
+        printf("%s\n", nm[x].nome);
+    }
+    for(int j=0;j<100-1;j++){
+        for(i=0; i<100-j-1; i++){
+            if(strcmp(nm[i].nome,nm[i+1].nome)>0){
+                strcpy(temp,nm[i].nome);
+                strcpy(nm[i].nome,nm[i+1].nome);
+                strcpy(nm[i+1].nome,temp);
+            }
+        }
+    }
+    printf("\n");
+    for(int x=0;x<100;x++){
+        printf("%s\n", nm[x].nome);
+    }
+    return 0;
+}
